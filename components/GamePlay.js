7936f9591fe0a625f6221d0874cc2cebbc8a77c5
Kelsey McAlpine <kmcalpine023@gmail.com>
2017-07-18 16:14:52 -0700

    Moved 'save scorecard' functionality to GamePlay. TODO: save scorecard to firebase after each hole of course has been played.

--- components/GamePlay.js
+++ components/GamePlay.js
@@ -3,9 +3,37 @@ import { connect } from 'react-redux';
 import HoleForm from './HoleForm';
 
 class GamePlay extends Component {
+  saveScorecard() {
+    // const { holeDetails } = this.props;
+    // const courseId = holeDetails[0].course_id;
+    // const holes = (holeDetails.length) - 1;
+    // const getDate = new Date();
+    // const currentDate = (currentDate.getMonth()+1) + '/'
+    //                 + currentDate.getDate() + '/'
+    //                 + currentDate.getFullYear();
+    //
+    // console.log(currentDate);
+
+    // const scorecardInfo = {
+    //   details: {
+    //     courseId,
+    //     holes,
+    //     datetime
+    //   },
+    //   playerNames: {
+    //     player1,
+    //     player2,
+    //     player3,
+    //     player4,
+    //   }
+    // };
+
+    // this.props.saveScorecard({ scorecardInfo });
+  }
+  
   render() {
-    console.log('in game play. Holedetails:', this.props.holeDetails);
-    console.log('in game plat. PLayers: ', this.props.players);
+    console.log('in game play. Hole details:', this.props.holeDetails);
+    console.log('in game play. Players: ', this.props.players);
     return (
       <HoleForm />
     );
