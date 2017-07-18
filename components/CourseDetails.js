f12b02e506d194decfab583d16ada73909da8582
Kelsey McAlpine <kmcalpine023@gmail.com>
2017-07-18 11:10:06 -0700

    Course Details shows map and marker pin based on hardcoded coordinates.

--- components/CourseDetails.js
+++ components/CourseDetails.js
@@ -1,103 +1,195 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { Text } from 'react-native';
-// need to add Image back in
-import { Button, Card, CardSection, Spinner } from '../components/common';
-import { courseDetailsFetch, createScorecardForm } from '../actions';
+import { View, ActivityIndicator } from 'react-native';
+import { MapView } from 'expo';
 
-
-class CourseDetails extends Component {
-
-  componentWillMount() {
-    console.log(this.props.courseId);
-    this.props.courseDetailsFetch({ courseId: this.props.courseId });
+class MapScreen extends Component {
+  // starting and zoom level for mapview
+  // lon and lat are center
+  state = {
+    mapLoaded: false,
+    region: {
+      longitude: -122,
+      latitude: 37,
+      longitudeDelta: 0.04,
+      latitudeDelta: 0.09
+    },
+    marker: {
+      coordinate: {
+        longitude: -122,
+        latitude: 37,
+      },
+      title: 'test',
+      description: 'this is so fun'
+    }
   }
 
-  onPressStart() {
-    console.log('1. pressed start button to create scorecard');
-    console.log('passing courseId: ', this.props.courseId);
-    this.props.createScorecardForm({ courseId: this.props.courseId });
+  componentDidMount() {
+    this.setState({ mapLoaded: true });
   }
 
-  constructImageUrl() {
-    const { course_photo_url_medium } = this.props.courseDetails;
-    const baseUrl = 'https://www.dgcoursereview.com/course_pics/';
-    const imageSrc = course_photo_url_medium.slice(42);
-
-    return `${baseUrl}${imageSrc}`;
-  }
-
-  displayPublicPrivate(privateStatus) {
-    if (privateStatus === 1) { return 'Private'; }
-    return 'Public';
+  onRegionChangeComplete = (region) => {
+    // console.log(region);
+    this.setState({ region });
   }
 
   render() {
-    if (!this.props.courseDetails) {
+    if (!this.state.mapLoaded) {
       return (
-          <Spinner />
+        <View style={{ flex: 1, justifyContent: 'center' }}>
+          <ActivityIndicator size='large' />
+        </View>
       );
     }
-
-    const { name, holes, rating } = this.props.courseDetails;
-    const privateStatus = this.props.courseDetails.private;
-
-    // <CardSection>
-    //   <Image source={{ uri: this.constructImageUrl() }} style={styles.imageStyle} />
-    // </CardSection>
     return (
-      <Card>
-
-        <CardSection>
-          <Text>{name} and rating: {rating}</Text>
-        </CardSection>
-
-        <CardSection>
-          <Text>
-            Decription placeholder text Located west of Rochester
-            N.Y. in the town of Riga. It is an 18 hole course with the
-            front 9 mostly open and park like and the back 9 weaving in
-            and out of the woods. "Woodsy" atmosphere and a few holes
-            where water comes into play.
-          </Text>
-        </CardSection>
-
-        <CardSection>
-          <Text>{this.displayPublicPrivate(privateStatus)}</Text>
-        </CardSection>
-
-        <CardSection>
-          <Text>{holes} Holes</Text>
-        </CardSection>
-
-        <CardSection>
-          <Text>Distance from current location</Text>
-        </CardSection>
-
-        <CardSection>
-          <Text>Map will go here</Text>
-        </CardSection>
-
-        <CardSection>
-          <Button onPress={this.onPressStart.bind(this)}>Start Game</Button>
-        </CardSection>
-      </Card>
+      <View style={{ flex: 1 }}>
+        <MapView
+          region={this.state.region}
+          style={{ flex: 1 }}
+          onRegionChangeComplete={this.onRegionChangeComplete}
+        >
+          <MapView.Marker
+            coordinate={this.state.marker.coordinate}
+            title={this.state.marker.title}
+            description={this.state.marker.description}
+          />
+        </MapView>
+      </View>
     );
   }
 }
 
-// const styles = {
-//   imageStyle: {
-//     height: 250,
-//     flex: 1,
-//     width: null,
-//   },
+export default MapScreen;
+
+
+// import React, { Component } from 'react';
+// import { connect } from 'react-redux';
+// import { Text, View, ActivityIndicator } from 'react-native';
+// import { MapView } from 'expo';
+// // need to add Image back in
+// import { Button, Card, CardSection, Spinner } from '../components/common';
+// import { courseDetailsFetch, createScorecardForm } from '../actions';
+//
+//
+// class CourseDetails extends Component {
+//
+//   state = {
+//     mapLoaded: false,
+//     region: {
+//       longitude: -122,
+//       latitude: 37,
+//       longitudeDelta: 0.04,
+//       latitudeDelta: 0.09
+//     }
+//   }
+//
+//   componentWillMount() {
+//     console.log(this.props.courseId);
+//     this.props.courseDetailsFetch({ courseId: this.props.courseId });
+//   }
+//
+//   componentDidMount() {
+//     this.setState({ mapLoaded: true });
+//   }
+//
+//   onPressStart() {
+//     console.log('1. pressed start button to create scorecard');
+//     console.log('passing courseId: ', this.props.courseId);
+//     this.props.createScorecardForm({ courseId: this.props.courseId });
+//   }
+//
+//   constructImageUrl() {
+//     const { course_photo_url_medium } = this.props.courseDetails;
+//     const baseUrl = 'https://www.dgcoursereview.com/course_pics/';
+//     const imageSrc = course_photo_url_medium.slice(42);
+//
+//     return `${baseUrl}${imageSrc}`;
+//   }
+//
+//   displayPublicPrivate(privateStatus) {
+//     if (privateStatus === 1) { return 'Private'; }
+//     return 'Public';
+//   }
+//
+//   onRegionChangeComplete = (region) => {
+//     // console.log(region);
+//     this.setState({ region });
+//   }
+//
+//   render() {
+//     if (!this.props.courseDetails || !this.state.mapLoaded) {
+//       return (
+//         <View style={{ flex: 1, justifyContent: 'center' }}>
+//           <ActivityIndicator size='large' />
+//         </View>
+//       );
+//     }
+//
+//     const { name, holes, rating, longitude, latitude } = this.props.courseDetails;
+//     const privateStatus = this.props.courseDetails.private;
+//
+//     // <CardSection>
+//     //   <Image source={{ uri: this.constructImageUrl() }} style={styles.imageStyle} />
+//     // </CardSection>
+//     return (
+//       <Card>
+//
+//         <CardSection>
+//           <Text>{name} and rating: {rating}</Text>
+//         </CardSection>
+//
+//         <CardSection>
+//           <Text>
+//             Decription placeholder text Located west of Rochester
+//             N.Y. in the town of Riga. It is an 18 hole course with the
+//             front 9 mostly open and park like and the back 9 weaving in
+//             and out of the woods. "Woodsy" atmosphere and a few holes
+//             where water comes into play.
+//           </Text>
+//         </CardSection>
+//
+//         <CardSection>
+//           <Text>{this.displayPublicPrivate(privateStatus)}</Text>
+//         </CardSection>
+//
+//         <CardSection>
+//           <Text>{holes} Holes</Text>
+//         </CardSection>
+//
+//         <CardSection>
+//           <Text>Distance from current location</Text>
+//         </CardSection>
+//
+//         <CardSection>
+//           console.log('1.', this.state);
+//           <View style={{ flex: 1 }}>
+//             <MapView
+//               region={this.state.region}
+//               style={{ flex: 1 }}
+//               onRegionChangeComplete={this.onRegionChangeComplete}
+//             />
+//           </View>
+//         </CardSection>
+//
+//         <CardSection>
+//           <Button onPress={this.onPressStart.bind(this)}>Start Game</Button>
+//         </CardSection>
+//       </Card>
+//     );
+//   }
+// }
+//
+// // const styles = {
+// //   imageStyle: {
+// //     height: 250,
+// //     flex: 1,
+// //     width: null,
+// //   },
+// // };
+//
+//
+// const mapStateToProps = state => {
+//   const { courseDetails } = state.courses;
+//   return { courseDetails };
 // };
-
-
-const mapStateToProps = state => {
-  const { courseDetails } = state.courses;
-  return { courseDetails };
-};
-
-export default connect(mapStateToProps, { courseDetailsFetch, createScorecardForm })(CourseDetails);
+//
+// export default connect(mapStateToProps, { courseDetailsFetch, createScorecardForm })(CourseDetails);
