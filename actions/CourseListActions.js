34aff439464aaa049bd5e18b49c0883005789304
Kelsey McAlpine <kmcalpine023@gmail.com>
2017-07-18 12:44:53 -0700

    Updated courseDetailsFetch and callback. Fixed bug with mock API call.

--- actions/CourseListActions.js
+++ actions/CourseListActions.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-// import { DG_API_KEY, DG_COURSE_DETAILS_SIG, DG_RADIUS_LIST_SIG } from 'react-native-dotenv';
+import { DG_API_KEY, DG_COURSE_DETAILS_SIG, DG_RADIUS_LIST_SIG } from 'react-native-dotenv';
 import { Actions } from 'react-native-router-flux';
 import {
   COURSE_LIST_FETCH_SUCCESS,
@@ -16,26 +16,29 @@ export const courseListFetch = (callback) => async (dispatch) => {
   }
 };
 
-export const courseDetailsFetch = () => {
-  return (dispatch) => {
-    axios.get('https://api.myjson.com/bins/11ejt7')
-    // add { courseId } to constructor
-    // axios.get('https://www.dgcoursereview.com/api_test/index.php', {
-    //     params: {
-    //       key: DG_API_KEY,
-    //       mode: 'crseinfo',
-    //       id: courseId,
-    //       sig: DG_COURSE_DETAILS_SIG
-    //     }
-    //   })
-      .then((response) => {
-        dispatch({
-          type: COURSE_DETAILS_FETCH_SUCCESS,
-          payload: response.data
-        });
-      })
-      .catch((err) => {
-        console.log('error: ', err);
-      });
-  };
+export const courseDetailsFetch = (courseId, callback) => async (dispatch) => {
+  try {
+    let { data } = await axios.get('https://api.myjson.com/bins/ny42n');
+      // , {
+      //   params: {
+      //     key: DG_API_KEY,
+      //     mode: 'crseinfo',
+      //     id: 8,
+      //     sig: DG_COURSE_DETAILS_SIG
+      //   }
+      // });
+    dispatch({ type: COURSE_DETAILS_FETCH_SUCCESS, payload: data });
+    callback();
+  } catch(e) {
+    console.error(e);
+  }
 };
+// add { courseId } to constructor
+// axios.get('https://www.dgcoursereview.com/api_test/index.php', {
+//     params: {
+//       key: DG_API_KEY,
+//       mode: 'crseinfo',
+//       id: courseId,
+//       sig: DG_COURSE_DETAILS_SIG
+//     }
+//   })
