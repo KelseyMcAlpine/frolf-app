1989dac1742b591c2fcb8a847c5f2b3e4ec1939e
Kelsey McAlpine <kmcalpine023@gmail.com>
2017-07-19 09:12:32 -0700

    Added FacebookReducer and removed AuthReducer

--- reducers/index.js
+++ reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
-import AuthReducer from './AuthReducer';
+// import AuthReducer from './AuthReducer';
 import CourseListReducer from './CourseListReducer';
 import ScorecardFormReducer from './ScorecardFormReducer';
+import FacebookReducer from './FacebookReducer';
 
 export default combineReducers({
-  auth: AuthReducer,
+  // auth: AuthReducer,
   courses: CourseListReducer,
   scorecardForm: ScorecardFormReducer,
+  facebook: FacebookReducer,
 });
