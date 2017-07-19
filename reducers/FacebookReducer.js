98bc91093d092e6526ed29d3f028499ddd55f9df
Kelsey McAlpine <kmcalpine023@gmail.com>
2017-07-19 09:20:51 -0700

    Renamed files

--- reducers/FacebookReducer.js
+++ reducers/FacebookReducer.js
@@ -3,8 +3,10 @@ import { FACEBOOK_LOGIN_SUCCESS, FACEBOOK_LOGIN_FAIL } from '../actions/types';
 export default function (state = {}, action) {
   switch (action.type) {
     case FACEBOOK_LOGIN_SUCCESS:
+      console.log('in facebook login success. token received')
       return { token: action.payload };
     case FACEBOOK_LOGIN_FAIL:
+      console.log('in facebook login failed. token null')
       return { token: null };
     default:
       return state;
