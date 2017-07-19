265fa713d15300bdc120689cfe00e48fe2b66965
Kelsey McAlpine <kmcalpine023@gmail.com>
2017-07-19 14:12:46 -0700

    Added case for USER_INFO_FETCH_SUCCESS type

--- reducers/FacebookReducer.js
+++ reducers/FacebookReducer.js
@@ -1,4 +1,8 @@
-import { FACEBOOK_LOGIN_SUCCESS, FACEBOOK_LOGIN_FAIL } from '../actions/types';
+import {
+  FACEBOOK_LOGIN_SUCCESS,
+  FACEBOOK_LOGIN_FAIL,
+  USER_INFO_FETCH_SUCCESS
+} from '../actions/types';
 
 export default function (state = {}, action) {
   switch (action.type) {
@@ -8,6 +12,9 @@ export default function (state = {}, action) {
     case FACEBOOK_LOGIN_FAIL:
       console.log('in facebook login failed. token null')
       return { token: null };
+    case USER_INFO_FETCH_SUCCESS:
+      console.log('in user info fetch success. user_info name?: ', action.payload.name);
+      return { ...state, user_info: action.payload };
     default:
       return state;
   }
