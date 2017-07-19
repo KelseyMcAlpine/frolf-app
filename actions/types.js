06d79863cb891ebd57e648f7518cdaa0b97ee946
Kelsey McAlpine <kmcalpine023@gmail.com>
2017-07-19 14:06:44 -0700

    Added USER_INFO_FETCH_SUCCESS type

--- actions/types.js
+++ actions/types.js
@@ -1,5 +1,6 @@
 export const FACEBOOK_LOGIN_SUCCESS = 'facebook_login_success';
 export const FACEBOOK_LOGIN_FAIL = 'facebook_login_fail';
+export const USER_INFO_FETCH_SUCCESS = 'user_info_fetch_success';
 
 export const COURSE_LIST_FETCH_SUCCESS = 'course_list_fetch';
 export const COURSE_DETAILS_FETCH_SUCCESS = 'course_details_fetch_success';
