e54c09611e597ac08eec27863bea0e63dd2d51a4
Kelsey McAlpine <kmcalpine023@gmail.com>
2017-07-19 09:15:04 -0700

    Removed actions related to AuthActions and added action types from FacebookActions

--- actions/types.js
+++ actions/types.js
@@ -1,8 +1,5 @@
-export const EMAIL_CHANGED = 'email_changed';
-export const PASSWORD_CHANGED = 'password_changed';
-export const LOGIN_USER_SUCCESS = 'login_user_success';
-export const LOGIN_USER_FAIL = 'login_user_fail';
-export const LOGIN_USER_START = 'login_user';
+export const FACEBOOK_LOGIN_SUCCESS = 'facebook_login_success';
+export const FACEBOOK_LOGIN_FAIL = 'facebook_login_fail';
 
 export const COURSE_LIST_FETCH_SUCCESS = 'course_list_fetch';
 export const COURSE_DETAILS_FETCH_SUCCESS = 'course_details_fetch_success';
