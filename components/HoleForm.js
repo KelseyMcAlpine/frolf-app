e0b63ded4307cfc0b53ea9fefa8f77c5a99031f6
Kelsey McAlpine <kmcalpine023@gmail.com>
2017-07-20 16:13:22 -0700

    Score Picker for each player in players array. Functionality WIP.

--- components/HoleForm.js
+++ components/HoleForm.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
-import { View, Text, Picker, Button } from 'react-native';
-import { Card, CardSection } from './common';
+import { View, Text, Picker } from 'react-native';
+import { Card, CardSection, Input } from './common';
 
 class HoleForm extends Component {
+  renderPlayerInputs() {
+    return this.props.players.map((player, index) => {
+      return (
+        <View style={{ flexDirection: 'row', flex: 1 }}>
+          <Text>Player</Text>
+          <Picker
+            selectedValue={this.props.shift}
+            onValueChange={console.log('value changed')}
+            style={{ flex: 1 }}
+          >
+            <Picker.Item label="1" value="1" />
+            <Picker.Item label="2" value="2" />
+            <Picker.Item label="3" value="3" />
+          </Picker>
+        </View>
+      );
+    });
+  }
 
   render() {
-    console.log('in render HoleForm. current hole:', this.props.currentHole);
-    console.log('in render HoleForm. holeDetails:', this.props.holeDetails);
-
     const currentHole = this.props.currentHole;
     const holeDetails = this.props.holeDetails;
 
@@ -24,63 +39,13 @@ class HoleForm extends Component {
         <CardSection>
           <Text>PAR: {holeDetails[currentHole].tee_1_par}</Text>
         </CardSection>
+
+        <CardSection>
+          {this.renderPlayerInputs()}
+        </CardSection>
       </Card>
     );
   }
 }
-          // <Picker
-          //   selectedValue={this.props.shift}
-          //   style={{ flex: 1 }}
-          // >
-          //   <Picker.Item label="1" value="1" />
-          //   <Picker.Item label="2" value="2" />
-          //   <Picker.Item label="3" value="3" />
-          //   <Picker.Item label="4" value="4" />
-          //   <Picker.Item label="5" value="5" />
-          //   <Picker.Item label="6" value="6" />
-          //   <Picker.Item label="7" value="7" />
-          // </Picker>
-          //
-          // <Text>Player2</Text>
-          // <Picker
-          //   selectedValue={this.props.shift}
-          //   style={{ flex: 1 }}
-          // >
-          //   <Picker.Item label="1" value="1" />
-          //   <Picker.Item label="2" value="2" />
-          //   <Picker.Item label="3" value="3" />
-          //   <Picker.Item label="4" value="4" />
-          //   <Picker.Item label="5" value="5" />
-          //   <Picker.Item label="6" value="6" />
-          //   <Picker.Item label="7" value="7" />
-          // </Picker>
-          //
-          // <Text>Player3</Text>
-          // <Picker
-          //   selectedValue={this.props.shift}
-          //   style={{ flex: 1 }}
-          // >
-          //   <Picker.Item label="1" value="1" />
-          //   <Picker.Item label="2" value="2" />
-          //   <Picker.Item label="3" value="3" />
-          //   <Picker.Item label="4" value="4" />
-          //   <Picker.Item label="5" value="5" />
-          //   <Picker.Item label="6" value="6" />
-          //   <Picker.Item label="7" value="7" />
-          // </Picker>
-          //
-          // <Text>Player4</Text>
-          // <Picker
-          //   selectedValue={this.props.shift}
-          //   style={{ flex: 1 }}
-          // >
-          //   <Picker.Item label="1" value="1" />
-          //   <Picker.Item label="2" value="2" />
-          //   <Picker.Item label="3" value="3" />
-          //   <Picker.Item label="4" value="4" />
-          //   <Picker.Item label="5" value="5" />
-          //   <Picker.Item label="6" value="6" />
-          //   <Picker.Item label="7" value="7" />
-          // </Picker>
 
 export default HoleForm;
