1e28884fb442d4b8180b26b25f9ee65924d27f12
Kelsey McAlpine <kmcalpine023@gmail.com>
2017-07-23 15:01:24 -0700

    Updated router styling

--- Router.js
+++ Router.js
@@ -6,15 +6,22 @@ import CourseDetails from './screens/CourseDetails';
 import ScorecardForm from './screens/ScorecardForm';
 import GamePlay from './screens/GamePlay';
 import WelcomeScreen from './screens/WelcomeScreen';
+import { MaterialIcons } from '@expo/vector-icons';
+
 
 const RouterComponent = () => {
+  const { sceneStyle } = styles;
   return (
-    <Router sceneStyle={{ paddingTop: 63, backgroundColor: '#EEEEEE' }}>
-      <Scene key="auth">
+    <Router>
+      <Scene
+        key="auth"
+        hideNavBar={true}
+      >
         <Scene
           initial
           key="welcome"
           component={WelcomeScreen}
+          hideNavBar={true}
         />
         <Scene
           key="facebookLogin"
@@ -22,30 +29,48 @@ const RouterComponent = () => {
         />
       </Scene>
 
-      <Scene key="main">
+      <Scene
+        key="main"
+        titleStyle={{ fontWeight: '600' }}
+      >
         <Scene
           key="courseList"
           component={CourseList}
           title="Nearby Courses"
+          onLeft={() => alert("left button")}
+          leftTitle={<MaterialIcons name="sort" size={24} color="#000" />}
+          onRight={() => alert("right button")}
+          rightTitle={<MaterialIcons name="search" size={24} color="#000" />}
+          sceneStyle={sceneStyle}
         />
         <Scene
           key="courseDetails"
           component={CourseDetails}
           title="Course Details"
+          sceneStyle={sceneStyle}
         />
         <Scene
           key="scorecardForm"
           component={ScorecardForm}
           title="New Scorecard"
+          sceneStyle={sceneStyle}
         />
         <Scene
           key="gamePlay"
           component={GamePlay}
           title="Game"
+          sceneStyle={sceneStyle}
         />
       </Scene>
     </Router>
   );
+};
+
+const styles = {
+  sceneStyle: {
+    paddingTop: 63,
+    backgroundColor: '#EEEEEE'
+  }
 };
 
 export default RouterComponent;
