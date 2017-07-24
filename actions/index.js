6d1147e8e1cc030653f852e6b81c485aa6b9d36b
Kelsey McAlpine <kmcalpine023@gmail.com>
2017-07-24 09:22:33 -0700

    Added export from actions/ScorecardsListActions

--- actions/index.js
+++ actions/index.js
@@ -2,3 +2,4 @@ export * from './CourseListActions';
 export * from './ScorecardFormActions';
 export * from './FacebookActions';
 export * from './GamePlayActions';
+export * from './ScorecardsListActions';
