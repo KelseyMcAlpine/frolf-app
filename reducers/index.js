e6171001ff727bd5d18df8f7eb4cd060bba1f1d7
Kelsey McAlpine <kmcalpine023@gmail.com>
2017-07-24 09:24:03 -0700

    Created reducer for scorecardsList

--- reducers/index.js
+++ reducers/index.js
@@ -3,10 +3,12 @@ import CourseListReducer from './CourseListReducer';
 import ScorecardFormReducer from './ScorecardFormReducer';
 import FacebookReducer from './FacebookReducer';
 import GamePlayReducer from './GamePlayReducer';
+import ScorecardsListReducer from './ScorecardsListReducer';
 
 export default combineReducers({
   courses: CourseListReducer,
   scorecardForm: ScorecardFormReducer,
   facebook: FacebookReducer,
-  gameScores: GamePlayReducer
+  gameScores: GamePlayReducer,
+  scorecardsList: ScorecardsListReducer
 });
