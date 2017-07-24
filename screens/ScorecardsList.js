6e318beeca043406e0ee81d050c6f76a73e3778f
Kelsey McAlpine <kmcalpine023@gmail.com>
2017-07-24 09:25:52 -0700

    Displaying scorecards list from firebase! TODO: Styling, display more info, possibly link each scorecard to more detailed view.

--- screens/ScorecardsList.js
+++ screens/ScorecardsList.js
@@ -1,17 +1,52 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { Text, View, TouchableOpacity } from 'react-native';
-// import { connect } from 'react-redux';
+import { connect } from 'react-redux';
+import { scorecardsFetch } from '../actions';
 // import { Actions } from 'react-native-router-flux';
-import { Card, CardSection, Input, Button, GrayButton } from '../components/common';
+import { Card, CardSection, Input, Button, GrayButton, Spinner } from '../components/common';
 
 class ScorecardsList extends Component {
 
-  render() {
-    return(
-      <Card>
-        <CardSection>
-          <Text>Scorecards List</Text>
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loadingScorecards: true,
+    };
+  }
+
+  componentWillMount() {
+    this.props.scorecardsFetch();
+  }
+
+  componentWillReceiveProps() {
+    this.setState({ loadingScorecards: false });
+  }
+
+  renderScorecards() {
+    return this.props.scorecards.map(scorecard => {
+      return (
+        <CardSection key={scorecard.uid}>
+          <View>
+            <Text>{scorecard.details.courseId}</Text>
+            <Text>{scorecard.details.currentDate}</Text>
+            <Text>{scorecard.details.holes}</Text>
+          </View>
         </CardSection>
+      );
+    });
+  }
+
+  render() {
+    if ( this.state.loadingScorecards == true ){
+      return <Spinner />
+    }
+
+    console.log('scorecards:', this.props.scorecards);
+    return (
+      <Card>
+        {this.renderScorecards()}
       </Card>
     )
   }
@@ -28,4 +63,12 @@ const styles = {
   },
 }
 
-export default ScorecardsList;
+const mapStateToProps = (state) => {
+  const scorecards = _.map(state.scorecardsList.history, (val, uid) => {
+    return { ...val, uid };
+  });
+
+  return { scorecards };
+};
+
+export default connect(mapStateToProps, { scorecardsFetch })(ScorecardsList);
