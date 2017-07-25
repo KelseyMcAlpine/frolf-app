0842343a1f1868321a14a34fccd232b5bd2f7ab5
Kelsey McAlpine <kmcalpine023@gmail.com>
2017-07-25 10:10:31 -0700

    Added case for action type CLEAR_SCORES_SUCCESS

--- reducers/GamePlayReducer.js
+++ reducers/GamePlayReducer.js
@@ -1,4 +1,4 @@
-import { SAVE_SCORES_SUCCESS } from '../actions/types';
+import { SAVE_SCORES_SUCCESS, CLEAR_SCORES_SUCCESS } from '../actions/types';
 
 const INITIAL_STATE = {};
 
@@ -6,6 +6,8 @@ export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case SAVE_SCORES_SUCCESS:
       return { ...state, [`hole_${action.payload.currentHole}`]: action.payload.scores };
+    case CLEAR_SCORES_SUCCESS:
+      return INITIAL_STATE
     default:
       return state;
   }
