8a87e039021642a8b6cce69752b5b1d4e5ca00c3
Kelsey McAlpine <kmcalpine023@gmail.com>
2017-07-25 11:43:15 -0700

    Added callback to saveScores

--- actions/GamePlayActions.js
+++ actions/GamePlayActions.js
@@ -1,14 +1,17 @@
 import { SAVE_SCORES_SUCCESS, CLEAR_SCORES_SUCCESS } from './types';
 import { Actions } from 'react-native-router-flux';
 
-export const saveScores = ({ currentHole, scores }) => {
-  return {
-    type: SAVE_SCORES_SUCCESS,
-    payload: {
-      currentHole,
-      scores
-    }
-  };
+export const saveScores = ({ currentHole, scores }, callback) => {
+  return (dispatch) => {
+    dispatch({
+      type: SAVE_SCORES_SUCCESS,
+      payload: {
+        currentHole,
+        scores
+      }
+    });
+    callback();
+  }
 };
 
 export const clearScores = () => {
